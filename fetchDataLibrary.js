ef62df820dd9a252ec95b8495376faa2fa6fa7e8
banjman81 <70820423+banjman81@users.noreply.github.com>
2022-01-27 15:15:36 -0600

    assignment 4

--- fetchDataLibrary.js
+++ fetchDataLibrary.js
@@ -1,12 +1,12 @@
 var fetchData = () => new Promise((resolve, reject) => {
   console.log('fetchingData from imaginary database')
   setTimeout(() => {
-       try {
+        try {
          // fetchingData from imaginary database
          if((Math.ceil(Math.random() * 2)) < 2){
-           throw new Error('Error!')
-         }
-         resolve({name: 'john', age:42})
+            throw new Error('Error!')
+          }
+          resolve({name: 'john', age:42})
         } catch(error) {
           reject(error);
         }
